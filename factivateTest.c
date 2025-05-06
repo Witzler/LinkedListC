e3c5ae510bbd3638f9f142673a01e633d84f86bd
root <root@Virtuoso.>
2025-05-06 22:24:01 +0200

    Unfinished factiwait

--- factivateTest.c
+++ factivateTest.c
@@ -7,71 +7,42 @@
 #include <wait.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    if (argc < 4) {
-        fprintf(stderr, "Usage: %s <PROG1> [ARGS...] <DELIMITER> <PROG2> [ARGS...]\n", argv[0]);
-        exit(EXIT_FAILURE);
+void main(void){
+    pid_t pid, wpid;
+    if ((pid = fork()) == -1) { /* error */
+        perror("Fork failed: ");
+        exit(1);
+    } else if (pid == 0) { /* first child */
+        char *arguments[]={"ls", "-l", "-a", NULL};
+        printf("child: PID [%i] PPID [%i]\n", getpid(), getppid());
+        execvp(arguments[0], arguments);
+        perror("Command failed: ");
+        exit(1);
+    } else { /* parent */
+        // does nothing here!
     }
-
-    // Delimiter suchen (z. B. ";")
-    int delimiter_index = -1;
-    for (int i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], ";") == 0) {
-            delimiter_index = i;
-            break;
+    if ((pid = fork()) == -1) { /* error */
+        perror("Fork failed: ");
+        exit(1);
+    } else if (pid == 0) { /* second child */
+        char *arguments[]={"ls", "-l", "-a", "/foobar", NULL};
+        printf("child: PID [%i] PPID [%i]\n", getpid(), getppid());
+        execvp(arguments[0], arguments);
+        perror("Command failed: ");
+        exit(1);
+    } else { /* parent */
+        int status;
+        printf("parent: PID [%i] CPID [%i]\n", getpid(), pid);
+        printf("Waiting for child .... \n");
+        for (int i=0; i<2; i++) { // wait for 2 processes!
+            wpid=waitpid(-1, &status, 0);
+            if(WIFEXITED(status)){
+                printf("[%i] child (PID: %i) terminated via exit(%d)\n", i, wpid, WEXITSTATUS(status));
+            } else if(WIFSIGNALED(status)) {
+                printf("[%i] child (PID: %i) terminated via signal [%d]\n", i, wpid, WTERMSIG(status));
+            } else {
+                printf("[%i] something different happened to the child!\n",i );
+            }
         }
     }
-
-    if (delimiter_index == -1 || delimiter_index == 1 || delimiter_index == argc - 1) {
-        fprintf(stderr, "Error: delimiter missing or in wrong position.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // Erste Befehlsliste vorbereiten
-    char **prog1_argv = malloc((delimiter_index) * sizeof(char *));
-    for (int i = 0; i < delimiter_index; ++i) {
-        prog1_argv[i] = argv[i + 1]; // argv[1..delimiter_index-1]
-    }
-    prog1_argv[delimiter_index - 1 + 1] = NULL; // NULL-terminiert
-
-    // Zweite Befehlsliste vorbereiten
-    int prog2_argc = argc - delimiter_index - 1;
-    char **prog2_argv = malloc((prog2_argc + 1) * sizeof(char *));
-    for (int i = 0; i < prog2_argc; ++i) {
-        prog2_argv[i] = argv[delimiter_index + 1 + i];
-    }
-    prog2_argv[prog2_argc] = NULL;
-
-    // Kindprozess 1
-    pid_t pid1 = fork();
-    if (pid1 == 0) {
-        execvp(prog1_argv[0], prog1_argv);
-        perror("execvp failed for PROGRAM 1");
-        exit(EXIT_FAILURE);
-    } else if (pid1 < 0) {
-        perror("fork failed for PROGRAM 1");
-        exit(EXIT_FAILURE);
-    }
-
-    // Kindprozess 2
-    pid_t pid2 = fork();
-    if (pid2 == 0) {
-        execvp(prog2_argv[0], prog2_argv);
-        perror("execvp failed for PROGRAM 2");
-        exit(EXIT_FAILURE);
-    } else if (pid2 < 0) {
-        perror("fork failed for PROGRAM 2");
-        exit(EXIT_FAILURE);
-    }
-
-    // Elternprozess wartet auf beide Kinder
-    int status;
-    waitpid(pid1, &status, 0);
-    waitpid(pid2, &status, 0);
-
-    // Ressourcen aufräumen
-    free(prog1_argv);
-    free(prog2_argv);
-
-    return 0;
 }
--- factiwait.c
+++ factiwait.c
@@ -10,7 +10,9 @@
 * These are all headers you need!
 */
 
-
+//gcc -std=c99 -Wall -pedantic -o activate factiwait.c
+//./activate ls -l -a . \; date
+//./activate ls -l -a /tmp \; sleep 5
 
 
 int main(int argc, char *argv[]) {
@@ -20,26 +22,93 @@ int main(int argc, char *argv[]) {
     * <PROGRAM 1> [<PARAMETER>...]
     * <PROGRAM 2> [<PARAMETER>...]
     */
+    char **arguments1= NULL; //arguments1[]
+    int delimiter_index1 = -1;
+    char **arguments2= NULL; //arguments2[]
+    int delimiter_index2 = -1;
 
+    //First PROGRAMM Preperation
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "\;") == 0) {
+            delimiter_index1 = i;
+            break;
+        }
+    }
+    arguments1 = malloc((delimiter_index1) * sizeof(char *) ); //A spot for NULL shall be included here
+    for (int i = 1; i < delimiter_index1; i++) {
+        int y = 0;
+        arguments1[y] = argv[i];
+        y++;
+    }
+    arguments1[delimiter_index1 -1] = NULL;
 
+    //Second PROGRAMM Preperation
+    delimiter_index2 = argc - delimiter_index1-1;
+    arguments2 = malloc((delimiter_index2+1) * sizeof(char *));
+    for (int i = delimiter_index2; i < argc; i++) {
+        int y = 0;
+        arguments1[y] = argv[i];
+        y++;
+    }
+    arguments2[delimiter_index2] = NULL;
+
+    //PIDs
+    pid_t pid, wpid;
 
     /*
     * create first child (fork()) and execute
     * <PROGRAM 1> [<PARAMETER>...] with exec*()
     */
-
+    if ((pid = fork()) == -1) { /* error */
+        perror("Fork failed: ");
+        exit(1);
+    }
+    if (pid == 0) { /* first child */
+        //char *arguments[]={"ls", "-l", "-a", NULL};
+        printf("child: PID [%i] PPID [%i]\n", getpid(), getppid());
+        execvp(arguments1[0], arguments1);
+        perror("Command failed: ");
+        exit(1);
+    } else { /* parent */
+        // does nothing here!
+        printf("Parent does nothing here\n");
+    }
 
 
     /*
     * create second child (fork()) and execute
     * <PROGRAM 1> [<PARAMETER>...] with exec*()
     */
-
-
+    if ((pid = fork()) == -1) { /* error */
+        perror("Fork failed: ");
+        exit(1);
+    }
+    if (pid == 0) { /* second child */
+        //char *arguments[]={"ls", "-l", "-a", "/foobar", NULL};
+        printf("child: PID [%i] PPID [%i]\n", getpid(), getppid());
+        execvp(arguments2[0], arguments2);
+        perror("Command failed: ");
+        exit(1);
+    }
     /*
     * Typically, here is the parent. Make sure this code is called
     * _after_ both subprocesses were launched!
     * Use wait() or waitpid() and wait until _both_ subprocesses are finished!
     * Then use exit()
     */
+    else { /* parent */
+        int status;
+        printf("parent: PID [%i] CPID [%i]\n", getpid(), pid);
+        printf("Waiting for child .... \n");
+        for (int i=0; i<2; i++) { // wait for 2 processes!
+            wpid=waitpid(-1, &status, 0);
+            if(WIFEXITED(status)){
+                printf("[%i] child (PID: %i) terminated via exit(%d)\n", i, wpid, WEXITSTATUS(status));
+            } else if(WIFSIGNALED(status)) {
+                printf("[%i] child (PID: %i) terminated via signal [%d]\n", i, wpid, WTERMSIG(status));
+            } else {
+                printf("[%i] something different happened to the child!\n",i );
+            }
+        }
+    }
 }
